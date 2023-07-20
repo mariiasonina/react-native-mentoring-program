ec984f5588cb1ef1fe49bb412df6bbe4c1846153
Mariya Sonina <mariya_sonina@epam.com>
2023-07-20 20:38:12 +0100

    Configure prettier

--- ReactNativeProject/.prettierrc.js
+++ ReactNativeProject/.prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+	arrowParens: 'avoid',
+	bracketSameLine: true,
+	bracketSpacing: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	endOfLine: 'auto',
+	tabWidth: 2,
+	bracketSpacing: true,
 };
