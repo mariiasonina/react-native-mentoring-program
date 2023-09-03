489544835ae5c63c6560cd112687c6a084a42b37
Mariya Sonina <mariya_sonina@epam.com>
2023-09-03 16:18:57 +0100

    Implement CI/CD

--- ReactNativeProject/src/components/ProductMainInfo/styles.ts
+++ ReactNativeProject/src/components/ProductMainInfo/styles.ts
@@ -1,9 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-	productName: {
-		color: '#000000',
-		fontWeight: '400',
-		fontSize: 15,
-	},
+  productName: {
+    color: '#000000',
+    fontWeight: '400',
+    fontSize: 15,
+  },
 });
